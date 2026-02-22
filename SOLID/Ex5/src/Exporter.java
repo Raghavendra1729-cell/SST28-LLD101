ddc0a6c4b1b7ee8b8a74c37131d5815e6f7471c2
lingaraghavendra <lingaraghawendra@gmail.com>
2026-02-22 17:45:30 +0530

    question 5

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,23 @@
 public abstract class Exporter {
-    // implied "contract" but not enforced (smell)
-    public abstract ExportResult export(ExportRequest req);
+
+    public final ExportResult export(ExportRequest req) {
+        if (req == null) {
+            return new ExportResult(getContentType(), new byte[0]);
+        }
+        
+
+        if (req.body != null && req.body.length() > getMaxContentLength()) {
+            throw new IllegalArgumentException(getFormatName() + " cannot handle content > " + getMaxContentLength() + " chars");
+        }
+        
+        return generateExport(req);
+    }
+    protected abstract ExportResult generateExport(ExportRequest req);
+
+    public int getMaxContentLength() {
+        return (int)1e9; 
+    }
+
+    public abstract String getFormatName();
+    public abstract String getContentType();
 }
