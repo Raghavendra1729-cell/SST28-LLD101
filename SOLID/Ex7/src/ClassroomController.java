caea31081c9bb15c07140ffaa62d2a1cc9128013
lingaraghavendra <lingaraghawendra@gmail.com>
2026-03-01 13:36:07 +0530

    ex7 done

--- SOLID/Ex7/src/ClassroomController.java
+++ SOLID/Ex7/src/ClassroomController.java
@@ -5,23 +5,25 @@ public class ClassroomController {
 
     public void startClass() {
         SmartClassroomDevice pj = reg.getFirstOfType("Projector");
-        pj.powerOn();
-        pj.connectInput("HDMI-1");
+        ((Power)pj).powerOn();
+        ((ConnectInput)(pj)).connectInput("HDMI-1");
 
         SmartClassroomDevice lights = reg.getFirstOfType("LightsPanel");
-        lights.setBrightness(60);
+        ((Brightness)lights).setBrightness(60);
 
         SmartClassroomDevice ac = reg.getFirstOfType("AirConditioner");
-        ac.setTemperatureC(24);
+        ((AirConditioner)ac).setTemperatureC(24);
 
         SmartClassroomDevice scan = reg.getFirstOfType("AttendanceScanner");
-        System.out.println("Attendance scanned: present=" + scan.scanAttendance());
+        System.out.println("Attendance scanned: present=" + ((Attendance)scan).scanAttendance());
     }
 
     public void endClass() {
         System.out.println("Shutdown sequence:");
-        reg.getFirstOfType("Projector").powerOff();
-        reg.getFirstOfType("LightsPanel").powerOff();
-        reg.getFirstOfType("AirConditioner").powerOff();
+        System.out.println("Shutdown sequence:");
+        // FIX: Cast these to (Power) so the compiler knows they have a powerOff() method
+        ((Power) reg.getFirstOfType("Projector")).powerOff();
+        ((Power) reg.getFirstOfType("LightsPanel")).powerOff();
+        ((Power) reg.getFirstOfType("AirConditioner")).powerOff();
     }
 }
