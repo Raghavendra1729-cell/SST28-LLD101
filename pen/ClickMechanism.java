3dd44540c8b6dde4dba2ff1eba3a065fdc507ebc
lingaraghavendra <lingaraghawendra@gmail.com>
2026-03-23 15:27:05 +0530

    Pen assignment

--- pen/ClickMechanism.java
+++ pen/ClickMechanism.java
@@ -1,13 +1,13 @@
-public class CapMechanism implements StartingMechanism {
+public class ClickMechanism implements StartingMechanism {
     @Override
     public void start(Pen pen) {
         pen.setStarted(true);
-        System.out.println("Cap opened.");
+        System.out.println("Pen is ready to write.");
     }
 
     @Override
     public void close(Pen pen) {
         pen.setStarted(false);
-        System.out.println("Cap closed.");
+        System.out.println("Pen is closed.");
     }
 }
